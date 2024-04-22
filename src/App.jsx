422badfaf211904388b89dd8d47eb56e11e872de
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-22 11:52:07 -0600

    Use Container and sx prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 
 export default function App() {
   return (
-    <>
-    <h1>Hello World</h1>
-    <Button variant="contained">My first button</Button>
-    </>
+    <Container sx={{ border: 3, boxShadow:3, pb: 2}}>
+      <h1>Hello World</h1>
+      <Button variant="contained">My first button</Button>
+    </Container>
   );
 }
