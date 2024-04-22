42b0ef1949b553c94bb01343bbf0f4137308b1a1
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-22 12:02:47 -0600

    Typography component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 
 export default function App() {
   return (
     <Container sx={{ border: 3, boxShadow:3, pb: 2}}>
       <h1>Hello World</h1>
+      <Typography variant="h1" component="h2">App h1 with component</Typography>
       <Button variant="contained">My first button</Button>
     </Container>
   );
