38c6537c7373deb6247d575234019ec0e7318586
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-22 11:42:55 -0600

    Button example and CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 export default function App() {
   return (
     <>
     <h1>Hello World</h1>
+    <Button variant="contained">My first button</Button>
     </>
   );
 }
