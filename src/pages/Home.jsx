3ddb731c98f996f4769624ecb9402c642562da2a
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-24 15:32:59 -0600

    Snackbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { Alert, AlertTitle, Box, Button, Collapse } from "@mui/material";
+import { Alert, AlertTitle, Box, Snackbar } from "@mui/material";
 import { useState } from "react";
-import AddReactionIcon from "@mui/icons-material/AddReaction";
+// import AddReactionIcon from "@mui/icons-material/AddReaction";
 
 export default function Home() {
   const [open, setOpen] = useState(true);
@@ -9,38 +9,12 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <Box sx={{ display: "grid", gap: "1rem" }}>
-        <Alert severity="error">
-          <AlertTitle>Error</AlertTitle>
-          This is an error alert
-        </Alert>
-        <Alert
-          severity="warning"
-          action={
-            <>
-              <Button color="inherit">Update</Button>
-              <Button color="inherit">Delete</Button>
-            </>
-          }
-        >
-          This is a warning alert
-        </Alert>
-        <Alert
-          severity="info"
-          icon={<AddReactionIcon />}
-          variant="outlined"
-        >
-          This is an info alert
-        </Alert>
-        <Collapse in={open}>
-          <Alert
-            severity="success"
-            onClose={() => {
-              setOpen(false);
-            }}
-          >
-            This is a success alert
+        <Snackbar open={open}>
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            This is an error alert
           </Alert>
-        </Collapse>
+        </Snackbar>
       </Box>
     </>
   );
